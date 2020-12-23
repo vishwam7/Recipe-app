a8b6613176c38b42117f9cea2390a4058078437d
[Vishwam Shah] <[shahvishwam7@gmail.com]>
2020-12-24 00:01:15 +0530

    initial setup of webpack, babel and plugins

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 // Global app controller
-import { x } from "./test";
-console.log(`I'm imported ${x} from another module`);
+import x from "./test";
+const y = 23;
+console.log(`I'm imported ${x} from another module called test.js! variable y is ${y}`);
